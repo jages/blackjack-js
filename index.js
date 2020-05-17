fed47e7da878213137628080ab317d3505031561
jages <16306035+jages@users.noreply.github.com>
2020-05-17 11:56:44 +0200

    score board and scoring logic

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let deck = []; //🃃
 shuffleDeck();
 let playerCards = [];
 let dealerCards = [];
+let playerScore = 0;
+let dealerScore = 0;
 
 const dealerToken = 'DEALER';
 const playerToken = 'PLAYER';
@@ -15,7 +17,10 @@ const cardBPend = '</span></div></div>';
 function initGame() {
     clearArea();
     shuffleDeck();
+    playerCards = [];
+    dealerCards = [];
     drawCard(dealerToken);
+    updatePoints();
 }
 
 function shuffleDeck() {
@@ -33,50 +38,85 @@ function shuffleDeck() {
     }
 }
 
+function calcScore() {
+    let getScore = function (cards) {
+        let ace = false;
+        let res = 0;
+        cards.forEach(x => {
+            if (x.num === 1) {
+                ace = true;
+                res += 11;
+            } else {
+                x.num >= 10 ? res += 10 : res += x.num;
+            }
+        });
+        if (res > 21 && ace)
+            res -= 10;
+
+        return res;
+    }
+    playerScore = getScore(playerCards);
+    dealerScore = getScore(dealerCards);
+
+}
+
 function drawCard(player) {
     let card = deck.splice(Math.floor(Math.random() * deck.length), 1)[0];
     console.log(card);
     player == dealerToken ? dealerCards.push(card) : playerCards.push(card);
 
     addCardToPlayArea(player, card);
+    updatePoints();
 }
 
 //visual helpers
-function getFaceDescription(i){
-    switch(i){
-        case 1: return 'A';
-        case 11: return 'J';
-        case 12: return 'Q';
-        case 13: return 'K';
-        default: return ''+i;
+function getFaceDescription(i) {
+    switch (i) {
+        case 1:
+            return 'A';
+        case 11:
+            return 'J';
+        case 12:
+            return 'Q';
+        case 13:
+            return 'K';
+        default:
+            return '' + i;
     }
 }
 
 
 
 // dom manipulation
-function clearArea(){
+function clearArea() {
     let cards = document.querySelectorAll('.dynamic-card');
     cards.forEach(card => card.remove());
+    updatePoints();
 }
 
-function addCardToPlayArea(player, card){
+function addCardToPlayArea(player, card) {
     let playArea = player == dealerToken ? document.querySelector('.dealer-cards') : document.querySelector('.player-cards');
-    let colorClass = card.suit =='♦' || card.suit == '♥' ? 'red' : 'black';
+    let colorClass = card.suit == '♦' || card.suit == '♥' ? 'red' : 'black';
     let cardHTML = cardBPstart + colorClass + cardBPmid + getFaceDescription(card.num) + card.suit + cardBPend;
     playArea.innerHTML += cardHTML;
 }
 
+function updatePoints(){
+    calcScore();
+    document.querySelector('#player-points').innerHTML = playerScore;
+    document.querySelector('#dealer-points').innerHTML = dealerScore;
+}
+
 //event handlers
 //document.querySelector('.card-stack').addEventListener('click', function(){console.log('got clicked')});
-document.addEventListener('keydown', function(sender){
-    if(sender.key === 'a'){
+document.addEventListener('keydown', function (sender) {
+    if (sender.key === 'a') {
         drawCard(dealerToken);
-    } else if(sender.key === 'd'){
+    } else if (sender.key === 'd') {
         drawCard(playerToken);
-    } else if(sender.key === 'x'){
+    } else if (sender.key === 'x') {
         initGame();
-    } else if(sender.key === 'y'){
+    } else if (sender.key === 'y') {
         clearArea();
     }
 });
